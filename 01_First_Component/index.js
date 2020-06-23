f98a21807f5b633ca3f4cc114dda9f4af6382b6b
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-23 13:52:24 +0500

    function vs class components

--- 01_First_Component/index.js
+++ 01_First_Component/index.js
@@ -1,13 +1,25 @@
-class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// Function vs Class Components
+// same as above:
+function Hello() {
+	return (
+		<div>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+		</div>
+	);
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
