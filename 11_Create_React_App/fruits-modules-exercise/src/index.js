b9b8d28d265ccb534dcd662ae20e93674bf568bb
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-25 15:52:38 +0500

    create react app - es2015 modules exercise - Colt's solution

--- 11_Create_React_App/fruits-modules-exercise/src/index.js
+++ 11_Create_React_App/fruits-modules-exercise/src/index.js
@@ -3,9 +3,9 @@ import { choice, remove } from './helpers';
 
 // console.log(foods);
 const randomFruit = choice(foods);
-console.log("I'd like one " + randomFruit + ', please');
-console.log('Here you go: ' + randomFruit);
+console.log(`I'd like one ${randomFruit}, please`);
+console.log(`Here you go: ${randomFruit}`);
 console.log('Delicious! May I have another?');
 // console.log(remove(foods, randomFruit));
 const fruitsLeft = remove(foods, randomFruit).length;
-console.log("I'm sorry, we're all out. We have " + fruitsLeft + ' left');
+console.log(`I'm sorry, we're all out. We have ${fruitsLeft} left`);
