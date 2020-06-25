bd828e455c051b56c68f18c816952ace9b2c76fd
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-25 18:02:41 +0500

    create react app - pokemon card app - better images

--- 12_Pokemon_Project/pokemon/src/Pokecard.js
+++ 12_Pokemon_Project/pokemon/src/Pokecard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'http://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// id needs to be three digits (001, 002, 043, 523, etc)
+let padToThree = (number) => (number <= 99 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
 	render() {
-		let imgSrc = `${POKE_API}${this.props.id}.png`;
+		let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
 		return (
 			<div className="Pokecard">
 				<h1 className="Pokecard-title">{this.props.name}</h1>
