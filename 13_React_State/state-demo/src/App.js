5d9a20d1ca952fa7eb4045a88bbf0d7d2aee51e7
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-26 09:47:36 +0500

    react state - setting state correctly

--- 13_React_State/state-demo/src/App.js
+++ 13_React_State/state-demo/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Game from './Game';
+import Rando from './Rando';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Game />
+				<Rando maxNum={7} />
 			</div>
 		);
 	}
