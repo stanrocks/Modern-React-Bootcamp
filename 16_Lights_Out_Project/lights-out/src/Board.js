3ad9a3ce9274bff02da145352fa81ba855f3a43b
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-05 00:19:13 +0500

    react lights-out - flip cells

--- 16_Lights_Out_Project/lights-out/src/Board.js
+++ 16_Lights_Out_Project/lights-out/src/Board.js
@@ -58,6 +58,7 @@ class Board extends Component {
 	/** handle changing a cell: update board & determine if winner */
 
 	flipCellsAround(coord) {
+		console.log('FLIPPING', coord);
 		let { ncols, nrows } = this.props;
 		let board = this.state.board;
 		let [ y, x ] = coord.split('-').map(Number);
@@ -71,11 +72,19 @@ class Board extends Component {
 		}
 
 		// TODO: flip this cell and the cells around it
+		// Flip initial cell
+		flipCell(y, x);
+		// Flip neighbors
+		flipCell(y, x - 1);
+		flipCell(y, x + 1);
+		flipCell(y - 1, x);
+		flipCell(y + 1, x);
 
 		// win when every cell is turned off
 		// TODO: determine is the game has been won
+		let hasWon = false;
 
-		// this.setState({ board, hasWon });
+		this.setState({ board, hasWon });
 	}
 
 	/** Render game board or winning message. */
@@ -92,7 +101,10 @@ class Board extends Component {
 		for (let y = 0; y < this.props.nrows; y++) {
 			let row = [];
 			for (let x = 0; x < this.props.ncols; x++) {
-				row.push(<Cell key={`${y}-${x}`} isLit={this.state.board[y][x]} />);
+				let coord = `${y}-${x}`;
+				row.push(
+					<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)} />
+				);
 			}
 			boardLayout.push(<tr key={y}>{row}</tr>);
 		}
