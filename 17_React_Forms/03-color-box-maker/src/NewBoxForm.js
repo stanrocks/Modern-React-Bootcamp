b6ee62d01bb20b1b9ef394d2801f5ec89b63126a
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-06 02:24:16 +0500

    react forms - color box maker exercise - form styling

--- 17_React_Forms/03-color-box-maker/src/NewBoxForm.js
+++ 17_React_Forms/03-color-box-maker/src/NewBoxForm.js
@@ -27,12 +27,18 @@ class NewBoxForm extends Component {
 	render() {
 		return (
 			<form onSubmit={this.handleSubmit}>
-				<label htmlFor="height">Height: </label>
-				<input id="height" name="height" value={this.state.height} onChange={this.handleChange} />
-				<label htmlFor="width">Width: </label>
-				<input id="width" name="width" value={this.state.width} onChange={this.handleChange} />
-				<label htmlFor="color">backgroundColor: </label>
-				<input id="color" name="color" value={this.state.color} onChange={this.handleChange} />
+				<div>
+					<label htmlFor="height">Height: </label>
+					<input id="height" name="height" value={this.state.height} onChange={this.handleChange} />
+				</div>
+				<div>
+					<label htmlFor="width">Width: </label>
+					<input id="width" name="width" value={this.state.width} onChange={this.handleChange} />
+				</div>
+				<div>
+					<label htmlFor="color">backgroundColor: </label>
+					<input id="color" name="color" value={this.state.color} onChange={this.handleChange} />
+				</div>
 				<button>Add a new box!</button>
 			</form>
 		);
