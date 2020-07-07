104d5535fede68b68a50b90b89a31360127e0fed
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-07 16:23:05 +0500

    react forms - todo app - Colt's solution

--- 17_React_Forms/04-todo-app/src/App.js
+++ 17_React_Forms/04-todo-app/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import TodoList from './TodoList';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoList from "./TodoList";
+import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<TodoList />
-		</div>
-	);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- 17_React_Forms/04-todo-app/src/Todo.js
+++ 17_React_Forms/04-todo-app/src/Todo.js
@@ -1,30 +1,80 @@
-import React, { Component } from 'react';
-import EditTodoForm from './EditTodoForm';
+import React, { Component } from "react";
+import "./Todo.css";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class Todo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { isEdited: false };
-		this.handleEdit = this.handleEdit.bind(this);
-	}
-
-	handleEdit() {
-		this.setState({
-			isEdited: true
-		});
-	}
-
-	render() {
-		const todoLayout = (
-			<span>
-				{' '}
-				{this.props.todo} <button onClick={this.handleEdit}>Edit</button>
-				<button onClick={this.props.removeTodo}>X</button>
-			</span>
-		);
-
-		return <div>{this.state.isEdited ? <EditTodoForm todo={this.props.todo} /> : todoLayout}</div>;
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleRemove() {
+    this.props.removeTodo(this.props.id);
+  }
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+  handleUpdate(evt) {
+    evt.preventDefault();
+    //take new task data and pass up to parent
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  handleToggle(evt) {
+    this.props.toggleTodo(this.props.id);
+  }
+  render() {
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <CSSTransition key='editing' timeout={500} classNames='form'>
+          <form className='Todo-edit-form' onSubmit={this.handleUpdate}>
+            <input
+              type='text'
+              value={this.state.task}
+              name='task'
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
+          </form>
+        </CSSTransition>
+      );
+    } else {
+      result = (
+        <CSSTransition key='normal' timeout={500} classNames='task-text'>
+          <li className='Todo-task' onClick={this.handleToggle}>
+            {this.props.task}
+          </li>
+        </CSSTransition>
+      );
+    }
+    return (
+      <TransitionGroup
+        className={this.props.completed ? "Todo completed" : "Todo"}
+      >
+        {result}
+        <div className='Todo-buttons'>
+          <button onClick={this.toggleForm}>
+            <i class='fas fa-pen' />
+          </button>
+          <button onClick={this.handleRemove}>
+            <i class='fas fa-trash' />
+          </button>
+        </div>
+      </TransitionGroup>
+    );
+  }
 }
-
 export default Todo;
