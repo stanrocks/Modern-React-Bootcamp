a780a963f3f9437685609cca4eb5614b9b0eb8b4
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-11 21:15:12 +0500

    react lifecycle - deck of cards - get new deck request to API

--- 19_React_Lifecycle/03-deck-of-cards/src/Deck.js
+++ 19_React_Lifecycle/03-deck-of-cards/src/Deck.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 class Deck extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { deck: '' };
+	}
+	componentDidMount() {
+		axios.get('https://deckofcardsapi.com/api/deck/new/shuffle').then((response) => {
+			this.setState({ deck: response.data });
+		});
+	}
 	render() {
 		return (
 			<div>
 				<h1>Deck</h1>
+				Remaining: {this.state.deck.remaining}
 				<Card />
 			</div>
 		);
