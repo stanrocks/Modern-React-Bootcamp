ee18943ab5d7b71bc4f53dd7a52da0e10766c9cb
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-11 20:51:21 +0500

    react lifecycle - deck of cards - basic structure

---
+++ 19_React_Lifecycle/03-deck-of-cards/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Deck from './Deck';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Deck />
+		</div>
+	);
+}
+
+export default App;
---
+++ 19_React_Lifecycle/03-deck-of-cards/src/Deck.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+class Deck extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Deck</h1>
+				<Card />
+			</div>
+		);
+	}
+}
+
+export default Deck;
