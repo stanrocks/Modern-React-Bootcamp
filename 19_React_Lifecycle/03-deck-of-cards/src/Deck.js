c0767317c09b470c7a8bec3716983d0a0d662e0f
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-12 23:47:58 +0500

    react lifecycle - deck of cards - draw new card

--- 19_React_Lifecycle/03-deck-of-cards/src/Deck.js
+++ 19_React_Lifecycle/03-deck-of-cards/src/Deck.js
@@ -5,18 +5,39 @@ import axios from 'axios';
 class Deck extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { deck: '' };
+		this.state = { deckId: '', cards: [], remaining: '' };
+		this.handleClick = this.handleClick.bind(this);
 	}
+
 	componentDidMount() {
+		// create new deck (consists 'deck_id' and 'remaining')
 		axios.get('https://deckofcardsapi.com/api/deck/new/shuffle').then((response) => {
-			this.setState({ deck: response.data });
+			this.setState({ deckId: response.data.deck_id });
 		});
 	}
+
+	drawCard() {
+		axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw`).then((response) => {
+			// console.log(response.data.remaining);
+			// console.log(response.data.cards[0].image);
+			this.setState({
+				remaining: response.data.remaining,
+				cards: [ ...this.state.cards, response.data.cards[0].image ]
+			});
+		});
+	}
+
+	handleClick() {
+		console.log('CLICKED');
+		this.drawCard();
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Deck</h1>
-				Remaining: {this.state.deck.remaining}
+				Remaining: {this.state.remaining}
+				<button onClick={this.handleClick}>GIMME A CARD!</button>
 				<Card />
 			</div>
 		);
