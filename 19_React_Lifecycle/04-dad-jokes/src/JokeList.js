ece375645874f6eceeb999ca60fadefcb910f5ee
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-16 02:47:59 +0500

    react lifecycle - dad jokes - sync with local storage

--- 19_React_Lifecycle/04-dad-jokes/src/JokeList.js
+++ 19_React_Lifecycle/04-dad-jokes/src/JokeList.js
@@ -11,11 +11,17 @@ class JokeList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			jokes: []
+			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
 		};
 	}
 
-	async componentDidMount() {
+	componentDidMount() {
+		if (this.state.jokes.length === 0) {
+			this.getJokes();
+		}
+	}
+
+	async getJokes() {
 		let jokes = [];
 		while (jokes.length < this.props.numJokesToGet) {
 			let res = await axios.get('https://icanhazdadjoke.com/', {
@@ -26,6 +32,7 @@ class JokeList extends Component {
 		}
 		// console.log(jokes);
 		this.setState({ jokes: jokes });
+		window.localStorage.setItem('jokes', JSON.stringify(jokes));
 	}
 
 	handleVote(id, delta) {
