1e936c23d8ecec1eebb1b131afdf3795e043fa94
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-17 19:30:22 +0500

    react lifecycle - dad jokes - loading screen

--- 19_React_Lifecycle/04-dad-jokes/src/JokeList.js
+++ 19_React_Lifecycle/04-dad-jokes/src/JokeList.js
@@ -11,7 +11,8 @@ class JokeList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
+			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+			loading: false
 		};
 		this.handleClick = this.handleClick.bind(this);
 	}
@@ -34,6 +35,7 @@ class JokeList extends Component {
 		// console.log(jokes);
 		this.setState(
 			(st) => ({
+				loading: false,
 				jokes: [ ...st.jokes, ...jokes ]
 			}),
 			() => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
@@ -50,10 +52,18 @@ class JokeList extends Component {
 	}
 
 	handleClick() {
-		this.getJokes();
+		this.setState({ loading: true }, this.getJokes);
 	}
 
 	render() {
+		if (this.state.loading) {
+			return (
+				<div className="JokeList-spinner">
+					<i className="far fa-8x fa-laugh fa-spin" />
+					<h1 className="JokeList-title">Loading...</h1>
+				</div>
+			);
+		}
 		return (
 			<div className="JokeList">
 				<div className="JokeList-sidebar">
