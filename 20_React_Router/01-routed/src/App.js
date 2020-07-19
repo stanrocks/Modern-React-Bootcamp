70b56b0815e8506ec9cfba8ae1329cfd0d2a6ea1
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-19 23:44:34 +0500

    react router - link component

--- 20_React_Router/01-routed/src/App.js
+++ 20_React_Router/01-routed/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import About from './About';
 import Dog from './Dog';
 import Contact from './Contact';
@@ -10,6 +10,16 @@ const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>;
 function App() {
 	return (
 		<div className="App">
+			<nav className="App-nav">
+				{/* link adds seamless experience - page doesn't reload */}
+				<Link to="/">About</Link>
+				<Link to="/dog">Dog</Link>
+				<Link to="/contact">Contact</Link>
+				{/* a uses GET request with page reloading*/}
+				<a href="/dog">Dog</a>
+				<a href="/contact">Contact</a>
+				<a href="/">About</a>
+			</nav>
 			{/* <Switch> */}
 			<Route exact path="/" component={About} />
 			<Route exact path="/dog" component={Dog} />
