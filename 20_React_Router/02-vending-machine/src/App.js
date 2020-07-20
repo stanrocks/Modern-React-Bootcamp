37d61c6403946d55ffa58c3926101c1281ea08ac
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-20 17:09:07 +0500

    react router - vending machine - navbar

--- 20_React_Router/02-vending-machine/src/App.js
+++ 20_React_Router/02-vending-machine/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Chips from './Chips';
 import Sardines from './Sardines';
 import Soda from './Soda';
+import Navbar from './Navbar';
 import VendingMachine from './VendingMachine';
 import { Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Switch>
 				<Route exact path="/" render={() => <VendingMachine />} />
 				<Route exact path="/chips" render={() => <Chips />} />
