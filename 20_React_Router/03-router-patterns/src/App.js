33b0cb7c4162611bb23c34fe5c9d7a024ea322ec
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-21 18:53:53 +0500

    react router patterns - 404 route

--- 20_React_Router/03-router-patterns/src/App.js
+++ 20_React_Router/03-router-patterns/src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import Food from './Food';
 import Meal from './Meal';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			{/* URL params */}
-			{/* <Food name="Kale" /> */}
-			{/* <Route exact path="/food/:name" render={(routeProps) => <Food name={routeProps.match.params.name} />} /> */}
-			{/* Have to spread 'match', 'location' and 'history' from props */}
-			{/* <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} /> */}
-			{/* React automatically pass in to component 'match', 'location' and 'history' as props */}
-			<Route exact path="/food/:name" component={Food} />
-			{/* If we need to pass something, we use render: */}
-			<Route exact path="/food/:name" render={(routeProps) => <Food authenticated={true} {...routeProps} />} />
+			<Switch>
+				{/* Home route */}
+				<Route exact path="/" render={() => <h1>Home Page!</h1>} />
 
-			{/* Multiplie Route Params */}
-			{/* try visit this: http://localhost:3000/food/potato/drink/wine */}
-			<Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+				{/* ========== */}
+				{/* URL params */}
+				{/* ========== */}
+
+				{/* <Food name="Kale" /> */}
+				{/* <Route exact path="/food/:name" render={(routeProps) => <Food name={routeProps.match.params.name} />} /> */}
+				{/* Have to spread 'match', 'location' and 'history' from props */}
+				{/* <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} /> */}
+				{/* React automatically pass in to component 'match', 'location' and 'history' as props */}
+				<Route exact path="/food/:name" component={Food} />
+				{/* If we need to pass something, we use render: */}
+				<Route exact path="/food/:name" render={(routeProps) => <Food authenticated={true} {...routeProps} />} />
+
+				{/* ===================== */}
+				{/* Multiple Route Params */}
+				{/* ===================== */}
+
+				{/* try visit this: http://localhost:3000/food/potato/drink/wine */}
+				<Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+
+				{/* =================== */}
+				{/* 404 Not Found Route */}
+				{/* =================== */}
+				{/* this is a route with no path declared */}
+				{/* have to use a switch, or else 404 will always render  */}
+				{/* that route should be at the end of switch, or it will intercept proper routes */}
+
+				{/* try visit any random path like http://localhost:3000/jojk */}
+				<Route render={() => <h1>Error! Not Found!</h1>} />
+			</Switch>
 		</div>
 	);
 }
