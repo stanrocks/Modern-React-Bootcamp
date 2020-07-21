162566060f0356a37129ebbba8d0f3392a41ed63
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-21 22:53:36 +0500

    react router patterns - redirect component

--- 20_React_Router/03-router-patterns/src/Food.js
+++ 20_React_Router/03-router-patterns/src/Food.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './Food.css';
 
 class Food extends Component {
@@ -7,8 +8,16 @@ class Food extends Component {
 		const url = `https://source.unsplash.com/1600x900/?${name}`;
 		return (
 			<div className="Food">
-				<h1>I love to eat {name}</h1>
-				<img src={url} alt={name} />
+				{/* check if there any digits in name: */}
+				{/\d/.test(name) ? (
+					// then redirect to home. else show food
+					<Redirect to="/" />
+				) : (
+					<div>
+						<h1>I love to eat {name}</h1>
+						<img src={url} alt={name} />
+					</div>
+				)}
 			</div>
 		);
 	}
