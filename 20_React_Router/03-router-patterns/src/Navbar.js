1e6aba65d8b829a427dba15c7d96a2fa680707d1
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-22 04:39:46 +0500

    react router patterns - back in history

--- 20_React_Router/03-router-patterns/src/Navbar.js
+++ 20_React_Router/03-router-patterns/src/Navbar.js
@@ -6,15 +6,20 @@ class Navbar extends Component {
 	constructor(props) {
 		super(props);
 		this.handleLogin = this.handleLogin.bind(this);
+		this.handleBack = this.handleBack.bind(this);
 	}
 	handleLogin() {
 		alert('Logged you in!');
 		this.props.history.push('/food/salmon');
 	}
+	handleBack() {
+		this.props.history.goBack();
+	}
 	render() {
 		return (
 			<div className="Navbar">
 				<button onClick={this.handleLogin}>Log In</button>
+				<button onClick={this.handleBack}>Go Back</button>
 			</div>
 		);
 	}
