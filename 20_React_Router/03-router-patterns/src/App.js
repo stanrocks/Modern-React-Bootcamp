445bc6b26443930050db216cf6efda46b8033d6a
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-07-21 14:56:36 +0500

    react router patterns - multiple route params

--- 20_React_Router/03-router-patterns/src/App.js
+++ 20_React_Router/03-router-patterns/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Food from './Food';
+import Meal from './Meal';
 import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
+			{/* URL params */}
 			{/* <Food name="Kale" /> */}
 			{/* <Route exact path="/food/:name" render={(routeProps) => <Food name={routeProps.match.params.name} />} /> */}
 			{/* Have to spread 'match', 'location' and 'history' from props */}
-			<Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} />
+			{/* <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} /> */}
 			{/* React automatically pass in to component 'match', 'location' and 'history' as props */}
 			<Route exact path="/food/:name" component={Food} />
 			{/* If we need to pass something, we use render: */}
 			<Route exact path="/food/:name" render={(routeProps) => <Food authenticated={true} {...routeProps} />} />
+
+			{/* Multiplie Route Params */}
+			{/* try visit this: http://localhost:3000/food/potato/drink/wine */}
+			<Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
 		</div>
 	);
 }
