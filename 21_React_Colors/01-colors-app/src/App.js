fdb563f2287859141ac2061695e6a65f253a102a
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-03 22:48:39 +0500

    colors app - finish mini palette delete

--- 21_React_Colors/01-colors-app/src/App.js
+++ 21_React_Colors/01-colors-app/src/App.js
@@ -16,11 +16,15 @@ class App extends Component {
 		};
 		this.findPalette = this.findPalette.bind(this);
 		this.savePalette = this.savePalette.bind(this);
+		this.deletePalette = this.deletePalette.bind(this);
 	}
 	findPalette(id) {
 		return this.state.palettes.find(function(palette) {
 			return palette.id === id;
 		});
+	}
+	deletePalette(id) {
+		this.setState((st) => ({ palettes: st.palettes.filter((palette) => palette.id !== id) }), this.syncLocalStorage);
 	}
 	savePalette(newPalette) {
 		this.setState({ palettes: [ ...this.state.palettes, newPalette ] }, this.syncLocalStorage);
@@ -39,7 +43,13 @@ class App extends Component {
 						<NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />
 					)}
 				/>
-				<Route exact path="/" render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps} />} />
+				<Route
+					exact
+					path="/"
+					render={(routeProps) => (
+						<PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps} />
+					)}
+				/>
 				<Route
 					exact
 					path="/palette/:id"
