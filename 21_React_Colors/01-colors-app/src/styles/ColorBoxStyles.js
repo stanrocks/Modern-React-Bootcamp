04554181f691bcb2474d2b0f07dcced735abdf3e
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-05 05:42:15 +0500

    colors app - fix color box styling

--- 21_React_Colors/01-colors-app/src/styles/ColorBoxStyles.js
+++ 21_React_Colors/01-colors-app/src/styles/ColorBoxStyles.js
@@ -9,7 +9,7 @@ export default {
 		display: 'inline-block',
 		position: 'relative',
 		cursor: 'pointer',
-		marginBottom: '-3.5px',
+		marginBottom: '-4px',
 		'&:hover button': {
 			opacity: 1
 		},
