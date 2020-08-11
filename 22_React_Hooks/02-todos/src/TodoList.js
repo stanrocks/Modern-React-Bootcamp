2b1f6f1e245324f593c835b37043262acee7aeb9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-12 03:54:01 +0500

    react hooks - todos - toggle completing and deletion

--- 22_React_Hooks/02-todos/src/TodoList.js
+++ 22_React_Hooks/02-todos/src/TodoList.js
@@ -5,13 +5,20 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
-function TodoList(props) {
+function TodoList({ todos, removeTodo, toggleTodo }) {
 	return (
 		<Paper>
 			<List>
-				{props.todos.map((todo) => (
+				{todos.map((todo) => (
 					<React.Fragment>
-						<Todo task={todo.task} key={todo.id} completed={todo.completed} />
+						<Todo
+							id={todo.id}
+							task={todo.task}
+							key={todo.id}
+							completed={todo.completed}
+							removeTodo={removeTodo}
+							toggleTodo={toggleTodo}
+						/>
 						<Divider />
 					</React.Fragment>
 				))}
