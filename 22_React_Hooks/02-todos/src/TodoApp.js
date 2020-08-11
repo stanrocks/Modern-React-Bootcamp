75f55c1afb2da8f365671d915a0011ccf1e80311
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-12 04:36:45 +0500

    react hooks - todos - editing functionality

--- 22_React_Hooks/02-todos/src/TodoApp.js
+++ 22_React_Hooks/02-todos/src/TodoApp.js
@@ -9,49 +9,61 @@ import Grid from '@material-ui/core/Grid';
 import { v4 as uuid } from 'uuid';
 
 function TodoApp() {
-	const initialTodos = [
-		{ id: 1, task: 'Clean Fishtank', completed: false },
-		{ id: 2, task: 'Wash Car', completed: true },
-		{ id: 3, task: 'Grow Beard', completed: false }
-	];
-	const [ todos, setTodos ] = useState(initialTodos);
-	const addTodo = (newTodoText) => {
-		setTodos([ ...todos, { id: uuid(), task: newTodoText, completed: false } ]);
-	};
-	const removeTodo = (todoId) => {
-		// filter out removed todo
-		const updatedTodos = todos.filter((todo) => todo.id !== todoId);
-		// call setTodos with new todos array
-		setTodos(updatedTodos);
-	};
-	const toggleTodo = (todoId) => {
-		const updatedTodos = todos.map((todo) => (todo.id === todoId ? { ...todo, completed: !todo.completed } : todo));
-		setTodos(updatedTodos);
-	};
-
-	return (
-		<Paper
-			style={{
-				padding: 0,
-				margin: 0,
-				height: '100vh',
-				backgroundColor: '#fafafa'
-			}}
-			elevation={0}
-		>
-			<AppBar color="primary" position="static" style={{ height: '64px' }}>
-				<Toolbar>
-					<Typography color="inherit">TODOS WITH HOOKS</Typography>
-				</Toolbar>
-			</AppBar>
-			<Grid container justify="center" style={{ marginTop: '1rem' }}>
-				<Grid item xs={11} md={8} lg={4}>
-					<TodoForm addTodo={addTodo} />
-					<TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
-				</Grid>
-			</Grid>
-		</Paper>
-	);
+  const initialTodos = [
+    { id: 1, task: 'Clean Fishtank', completed: false },
+    { id: 2, task: 'Wash Car', completed: true },
+    { id: 3, task: 'Grow Beard', completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
+  };
+  const removeTodo = (todoId) => {
+    // filter out removed todo
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    // call setTodos with new todos array
+    setTodos(updatedTodos);
+  };
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+  const editTodo = (todoId, newTask) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, task: newTask } : todo
+    );
+    setTodos(updatedTodos);
+  };
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa',
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: '64px' }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ marginTop: '1rem' }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 
 export default TodoApp;
--- 22_React_Hooks/02-todos/src/TodoList.js
+++ 22_React_Hooks/02-todos/src/TodoList.js
@@ -5,26 +5,27 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
-function TodoList({ todos, removeTodo, toggleTodo }) {
-	return (
-		<Paper>
-			<List>
-				{todos.map((todo) => (
-					<React.Fragment>
-						<Todo
-							id={todo.id}
-							task={todo.task}
-							key={todo.id}
-							completed={todo.completed}
-							removeTodo={removeTodo}
-							toggleTodo={toggleTodo}
-						/>
-						<Divider />
-					</React.Fragment>
-				))}
-			</List>
-		</Paper>
-	);
+function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
+  return (
+    <Paper>
+      <List>
+        {todos.map((todo) => (
+          <>
+            <Todo
+              id={todo.id}
+              task={todo.task}
+              key={todo.id}
+              completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
+  );
 }
 
 export default TodoList;
