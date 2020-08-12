41e5b0f10928ee5d1d9e84df103542fb0eef8939
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-12 05:22:47 +0500

    react hooks - todos - LocalStorage w UseEffect hook

--- 22_React_Hooks/02-todos/src/TodoApp.js
+++ 22_React_Hooks/02-todos/src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Typography from '@material-ui/core/Typography';
@@ -9,12 +9,18 @@ import Grid from '@material-ui/core/Grid';
 import { v4 as uuid } from 'uuid';
 
 function TodoApp() {
-  const initialTodos = [
-    { id: 1, task: 'Clean Fishtank', completed: false },
-    { id: 2, task: 'Wash Car', completed: true },
-    { id: 3, task: 'Grow Beard', completed: false },
-  ];
+  const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]');
+  // const initialTodos = [
+  //   { id: 1, task: 'Clean Fishtank', completed: false },
+  //   { id: 2, task: 'Wash Car', completed: true },
+  //   { id: 3, task: 'Grow Beard', completed: false },
+  // ];
   const [todos, setTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   const addTodo = (newTodoText) => {
     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
   };
