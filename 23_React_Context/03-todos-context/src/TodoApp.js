3a1d936074bc4212712f1f08e75986057aaf5c46
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-14 19:49:52 +0500

    react todos app - adding in todos context

---
+++ 23_React_Context/03-todos-context/src/TodoApp.js
@@ -0,0 +1,68 @@
+import React from 'react';
+// import useLocalStorageState from './hooks/useLocalStorageState';
+
+import useTodoState from './hooks/useTodoState';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+import { TodosProvider } from './contexts/todos.context';
+
+function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: 'Clean Fishtank', completed: false },
+    { id: 2, task: 'Wash Car', completed: true },
+    { id: 3, task: 'Grow Beard', completed: false },
+  ];
+  const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
+    initialTodos
+  );
+
+  // other example (to reuse useLocalStorageState hooks):
+  // const [mood, setMood] = useLocalStorageState('mood', 'happy');
+  // console.log(mood);
+
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa',
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: '64px' }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ marginTop: '1rem' }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodosProvider>
+            <TodoForm addTodo={addTodo} />
+            <TodoList
+              todos={todos}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+          </TodosProvider>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
+
+export default TodoApp;
+
+// - TodoApp
+//   - TodoForm
+//   - TodoList
+//     - TodoItem
+
+// each todo should have:
+// id, task, completed
