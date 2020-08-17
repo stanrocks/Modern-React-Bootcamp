3913c371af60d69c6cd56ba4c85fcecdac055849
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-18 00:00:00 +0500

    next - use withRouter instead of query to get id

--- 24_Next/01-first-next/pages/post.js
+++ 24_Next/01-first-next/pages/post.js
@@ -1,8 +1,15 @@
+import { withRouter } from 'next/router';
 import MyLayout from '../layouts/MyLayout';
 
-const Post = ({ id }) => {
-  return <h1>YOU ARE LOOKING AT POST #{id}</h1>;
-};
+// option 1:
+// const Post = ({ id }) => {
+//   return <h1>YOU ARE LOOKING AT POST #{id}</h1>;
+// };
+
+// option 2:
+const Post = withRouter((props) => {
+  return <h1>YOU ARE LOOKING AT POST #{props.router.query.id}</h1>;
+});
 
 Post.getInitialProps = async ({ query }) => {
   // console.log(query);
