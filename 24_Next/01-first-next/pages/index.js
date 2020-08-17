21479e2aa7434635cc330c6bcda78531432e9807
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-17 16:53:40 +0500

    next - intro to getStaticProps

--- 24_Next/01-first-next/pages/index.js
+++ 24_Next/01-first-next/pages/index.js
@@ -1,14 +1,32 @@
-// import Navbar from '../components/Navbar';
 import MyLayout from '../layouts/MyLayout';
 
 const Index = () => {
   console.log('RUNNING INDEX COMPONENT');
   return (
     <div>
-      {/* <Navbar /> */}
       <h1>Our Index Page!!!</h1>
     </div>
   );
 };
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
+  console.log('FETCHING YOUR DATA INSIDE GETSTATICPROPS');
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+
+  return {
+    props: {},
+  };
+}
+
 export default Index;
 Index.Layout = MyLayout;
