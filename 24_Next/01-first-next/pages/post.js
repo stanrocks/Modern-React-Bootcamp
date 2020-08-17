d7b966052ab4949700f180d6e8fc9f5f9dbc543f
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-08-18 00:53:25 +0500

    next - fetching comments

--- 24_Next/01-first-next/pages/post.js
+++ 24_Next/01-first-next/pages/post.js
@@ -1,19 +1,38 @@
-import { withRouter } from 'next/router';
+// import { withRouter } from 'next/router';
+import axios from 'axios';
 import MyLayout from '../layouts/MyLayout';
 
 // option 1:
-// const Post = ({ id }) => {
-//   return <h1>YOU ARE LOOKING AT POST #{id}</h1>;
-// };
+const Post = ({ id, comments }) => {
+  return (
+    <div>
+      <h1>Comments for Post #{id}</h1>
+      {comments.map((comment) => (
+        <Comment {...comment} key={comment.id}></Comment>
+      ))}
+    </div>
+  );
+};
 
+const Comment = ({ email, body }) => (
+  <div>
+    <h5>{email}</h5>
+    <p>{body}</p>
+  </div>
+);
 // option 2:
-const Post = withRouter((props) => {
-  return <h1>YOU ARE LOOKING AT POST #{props.router.query.id}</h1>;
-});
+// const Post = withRouter((props) => {
+//   return <h1>YOU ARE LOOKING AT POST #{props.router.query.id}</h1>;
+// });
 
 Post.getInitialProps = async ({ query }) => {
   // console.log(query);
-  return query;
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const { data } = res;
+  console.log(data[0]);
+  return { ...query, comments: data };
 };
 
 export default Post;
